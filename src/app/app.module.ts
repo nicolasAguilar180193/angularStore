c538356fefe0889d84fb4561c23780efd5f2a463
Nicolas <nicolas.aguilar180193@gmail.com>
2024-04-11 17:47:34 -0300

    Building home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MaterialModule } from './material.module';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsHeaderComponent } from './pages/home/components/products-header/products-header.component';
+import { FiltersComponent } from './pages/home/components/filters/filters.component';
+import { ProductsBoxComponent } from './pages/home/components/products-box/products-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    ProductsHeaderComponent,
+    FiltersComponent,
+    ProductsBoxComponent
   ],
   imports: [
     AppRoutingModule,
